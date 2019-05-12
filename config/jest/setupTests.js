3b74eddfee9d4ed642b9cd55249f934cd1ee15db
shurik <stu.shurik@gmail.com>
2019-05-12 13:36:01 +0300

    cleanup

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -2,10 +2,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-process.env.REACT_APP_EMISSARY_URL = String(Math.random());
-
-const root = document.createElement('div');
-root.setAttribute('id', 'root');
-
-document.body.appendChild(root);
